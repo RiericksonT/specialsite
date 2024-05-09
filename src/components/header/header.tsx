5553d6d39e5b6ed478a23d7dcbf12bec90c3fc68
Riericksonn <riericksontorres@grupomoura.com>
2024-05-09 14:49:56 -0300

    feat: adding images

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,11 +4,7 @@ function Header() {
   return (
     <header className={styles.header}>
       <a href="#">
-        <img
-          src="https://private-user-images.githubusercontent.com/53712152/326899641-a17cb4ff-4533-488b-84bd-4f625ba60e6d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQ1MDQ0NzUsIm5iZiI6MTcxNDUwNDE3NSwicGF0aCI6Ii81MzcxMjE1Mi8zMjY4OTk2NDEtYTE3Y2I0ZmYtNDUzMy00ODhiLTg0YmQtNGY2MjViYTYwZTZkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MzAlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDMwVDE5MDkzNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWZiMmUxYzcwMTA5MDQ2ZWZjOTE0OTc5ZmE1MTE1YTNjNDdlODg5ZTc3ZTZiNzc1MDlmNDE5NjY1MWI5YzU3Y2YmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.5KQBWU9lM1zjeVKvJOPp3DSDt5XFUDUtlNNKKHJltzo"
-          alt="Logo"
-          className={styles.logo}
-        />
+        <img src="/images/logo.png" alt="Logo" className={styles.logo} />
       </a>
       <nav className={styles.nav}>
         <ul className={styles.navList}>
--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -15,7 +15,7 @@ function Intro() {
       </div>
       <div className={styles.introRight}>
         <img
-          src="https://private-user-images.githubusercontent.com/53712152/326920264-63d41db3-0cb8-46da-a2ee-d1c05e30361a.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQ1MDQ0NDUsIm5iZiI6MTcxNDUwNDE0NSwicGF0aCI6Ii81MzcxMjE1Mi8zMjY5MjAyNjQtNjNkNDFkYjMtMGNiOC00NmRhLWEyZWUtZDFjMDVlMzAzNjFhLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MzAlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDMwVDE5MDkwNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWZmMGM0NzI1YjljODBhZTJlMGQ4MzNiZjhhYjQ4ZDU1NzQ2N2I5ZDk1YTIxNjk5Y2E5YTYxZmJiYmRkNDIzMzEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.6yw9N2czP4tQP6KLCFCp0KmwWbxGHROtNDjR39yLNj0"
+          src="/images/body-img.png"
           alt="Intro"
           className={styles.introImg}
         />
